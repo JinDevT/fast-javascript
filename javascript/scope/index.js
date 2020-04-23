fc8cee4eba4b5609085bd959412bec0a2560d75a
Kjintae <wlsxo2112@naver.com>
2020-04-23 18:02:34 +0900

    scope last commit

--- javascript/scope/index.js
+++ javascript/scope/index.js
@@ -1,17 +1,14 @@
-const teamName = '리버풀';
-const playerName = '반다이크';
-let position = '수비수';
+function logSkyColor() {
+    const dusk = true;
+    let myColor = 'blue'; 
+  
+    if (dusk) {
+      let myColor = 'pink';
+      console.log(myColor); // pink
+    }
+  
+    console.log(myColor); // blue 
+  }
 
-const playerInfo = () => {
-  position = '골키퍼';
-  return '선수 정보: ' + teamName + '의 ' + playerName + '는 '  + position + '입니다.';
-
-};
-
-const test = () => {
-    return position;
-}
-console.log(playerInfo()); // 선수 정보: 리버풀의 반다이크는 골키퍼 입니다.
-console.log(position); // 골키퍼
-
-console.log('test함수의 position', test());
+  logSkyColor();
+  
